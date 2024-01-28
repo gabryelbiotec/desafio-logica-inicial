f59a80bc9b37a4a8e6392e3b8b4dfb9f99ee4060
gabryelbiotec <gabryelmjj@gmail.com>
2024-01-27 23:47:20 -0300

    desafio-final

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
-console.log("Olá mundo");
-alert("Olá mundo")
+// Obtendo o nome e a quantidade de experiência do herói
+let nomeHeroi = "Amon";
+let xpHeroi = 10001
+// Utilizando uma estrutura de decisão para determinar o nível
+let nivel;
+
+if (xpHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+// Exibindo a mensagem final
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel);
